fa9d0475a7612679277a2c52fbf5ab40990480b8
SanaAsaf <sanaasaf@gmail.com>
2023-06-12 21:42:09 +0100

    Updates Header.jsx
    Sana's weather App message is shown

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import './Header.scss'
 const Header = ({title}) =>
     <header className="c-site-header">
         <h1 className="c-site-header__title o-type__invisible">{title}</h1>
+        <h2>Sana's Weather App</h2>
         {/* look up component */}
     </header>
 
